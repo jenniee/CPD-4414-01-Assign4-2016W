1a225ca5e9f287e4825111e7cd452bc424442c7d
c0637089 <c0637089@SB-N208-18.lambton.on.ca>
2016-02-24 10:39:59 -0500

    initial commit

--- src/java/servlet/AccountServlet.java
+++ src/java/servlet/AccountServlet.java
@@ -16,13 +16,44 @@
 
 package servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Account;
 
 /**
  * Provides an Account Balance and Basic Withdrawal/Deposit Operations
  */
 @WebServlet("/account")
 public class AccountServlet extends HttpServlet {
+    Account instance = new Account();
     
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response){
+        response.setHeader("Cache-Control", "private, no-store, no-cache, must-revalidate");
+        response.setHeader("Pragma", "no-cache");
+        response.setDateHeader("Expires", 0);
+        
+        try (PrintWriter out = response.getWriter()){
+            out.println(instance.getBalance());
+        } catch (IOException ex){
+            System.err.println("Could not retrieve Account information.");
+        }
+    }
+    public void doPost(HttpServletRequest request, HttpServletResponse response){
+        String withdraw = request.getParameter("withdraw");
+        String deposit = request.getParameter("deposit");
+        String close= request.getParameter("close");
+        
+        if(withdraw != null){
+            instance.withdraw(Double.valueOf(withdraw));
+        } else if (deposit != null){
+            instance.deposit(Double.valueOf(deposit));
+        } else if (close != null && close.equals("true")){
+            instance.close();
+        }
+    }
 }
